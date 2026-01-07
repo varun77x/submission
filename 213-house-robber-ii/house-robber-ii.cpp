abd9e67c8f821692cafd7763e5047206ff8f961e
Varun Deep Singh <129683022+varun77x@users.noreply.github.com>
2026-01-07 22:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (37.04%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+
+    int max_sum(vector<int>& nums,vector<int> dp,int l ,int r){
+         for(int i = l ; i <= r ; i++ ){
+
+            int skip = 0;
+            if(i - 1 >= 0){
+                skip = dp[i-1];
+            }
+            int take = nums[i];
+            if(i-2 >= 0){
+                take += dp[i-2];
+            }
+            dp[i] = max(skip,take);
+            
+        }
+
+        return dp[r];
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1,0);
@@ -7,33 +25,7 @@ public:
             return nums[0];
         }
 
-        for(int i = 1 ; i < n ; i++ ){
-            int skip = dp[i-1];
-            int take = nums[i];
-            if(i-2 >= 0){
-                take += dp[i-2];
-            }
-            dp[i] = max(skip,take);
-        }
-        int max_from_not_taking_the_first_element = dp[n-1];
-        
-        fill(dp.begin(),dp.end(),0);
-        for(int i = 0 ; i < n-1 ; i++ ){
-            int skip = 0;
-            if(i - 1 >= 0){
-                skip = dp[i-1];
-            }
-
-            int take = nums[i];
-            if(i-2 >= 0){
-                take += dp[i-2];
-            }
-
-            dp[i] = max(skip,take);
-        }
-        int max_from_taking_the_first_element = dp[n-2];
-
-        return max(max_from_not_taking_the_first_element,max_from_taking_the_first_element);
+        return max(max_sum(nums,dp,0,n-2), max_sum(nums,dp,1,n-1));
         
     }
 };
