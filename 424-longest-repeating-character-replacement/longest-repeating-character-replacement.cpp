babac714c37b2c461d8fd6b6166e23abf9d7df92
Varun Deep Singh <129683022+varun77x@users.noreply.github.com>
2026-01-21 02:00:53 +0530

    Time: 6 ms (48.79%) | Memory: 11.2 MB (7.23%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        // AABABABBA
+        int n = s.length();
+        int maxi = 0;
+        int left = 0;
+        int right = 0;
+        unordered_map<char, int> mp;
+        int curr = 0;
+
+        while (right < n) {
+            mp[s[right]]++;
+            int temp = 0;
+            for (auto& [key, value] : mp) {
+                temp = max(temp, value);
+            }
+            if ((right - left + 1) - temp > k) {
+                while ((right - left + 1) - temp > k and left < right) {
+                    mp[s[left]]--;
+                    left++;
+                    // for (auto& [key, value] : mp) {
+                    //     temp = max(temp, value);
+                    // }
+                }
+            } else
+                maxi = max(maxi, right - left + 1);
+            right++;
+        }
+
+        return maxi;
+    }
+};
